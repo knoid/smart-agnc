9e4840ded1cde362c190ec4a72dff66b7cb7b2f4
Ariel Barabas <smart-agnc@knoid.me>
2015-01-19 12:26:47 -0300

    only include png files into dist package

--- setup.py
+++ setup.py
@@ -1,19 +1,45 @@
-from distutils.command.install_data import install_data as InstallDataBuild
+from distutils.command.build_scripts import build_scripts
+from distutils.command.install_data import install_data
 from distutils.core import setup
 import os
+import platform
+from subprocess import call
+
+name = 'smart-agnc'
+package = name.replace('-', '_')
 
 
-class ChangeArchitecture(InstallDataBuild):
+class PreBuildScripts(build_scripts):
+    def run(self):
+        call('make')
+        build_scripts.run(self)
+
+
+class PreInstall(install_data):
+
+    data_files = []
+
     def run(self):
         with open('debian/control', 'r') as f:
             lines = f.readlines()
         with open('debian/control', 'w') as f:
             for line in lines:
                 if line.startswith('Architecture'):
-                    line = 'Architecture: amd64\n'
+                    if '64' in platform.processor():
+                        line = line.replace('all', 'amd64')
+                    else:
+                        line = line.replace('all', 'i386')
                 f.write(line)
+        data_files = []
+        for path, files in self.data_files:
+            files = [fpath for fpath in files if not
+                     (fpath.endswith('.svg') or fpath.endswith('.po'))]
+            if len(path) > 0 and not path.startswith('src'):
+                data_files.append((path, files))
+        self.data_files = data_files
 
-        InstallDataBuild.run(self)
+        print self.data_files
+        install_data.run(self)
 
 
 def get_files(path, prefix=''):
@@ -21,27 +47,28 @@ def get_files(path, prefix=''):
     for root, subdirs, files in os.walk(path):
         dir_files = []
         for filename in files:
-            if not filename.endswith('.po') and not filename.endswith('.svg'):
-                dir_files.append(os.path.join(root, filename))
+            dir_files.append(os.path.join(root, filename))
         if len(dir_files) > 0:
             all_files.append((os.path.join(prefix, root), dir_files))
     return all_files
 
 share_files = get_files('share', '/usr')
+share_files += [('', ['Makefile']), get_files('src/c-bind', '')]
 
 setup(
-    name='smart-agnc',
+    name=name,
     version='0.0.7',
     description='AT&T Global Network Client on steroids',
+    #long_description=readme,
     author='Ariel Barabas',
-    author_email='smart-agnc@knoid.me',
+    author_email='ariel.baras@gmail.com',
     url='https://github.com/knoid/smart-agnc',
     download_url='https://github.com/knoid/smart-agnc/releases',
-    packages=['smart_agnc'],
+    packages=[package],
     scripts=['bin/smart-agnc',
              'bin/sagnc-service-restart', 'bin/sagnc-bind'],
     license='GNUv2',
-    cmdclass={'install_data': ChangeArchitecture},
+    cmdclass={'build_scripts': PreBuildScripts, 'install_data': PreInstall},
     data_files=share_files,
-    package_dir={'smart_agnc': os.path.join('src', 'smart_agnc')}
+    package_dir={package: os.path.join('src', package)}
 )
