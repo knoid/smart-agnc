1a40315d82251a1601a123f9fe8c093330e9fce5
Ariel Barabas <smart-agnc@knoid.me>
2015-01-21 11:58:33 -0300

    added help text for --exit-button option

--- src/smart_agnc/__main__.py
+++ src/smart_agnc/__main__.py
@@ -17,7 +17,8 @@ gettext.install(__title__, i18n_dir)
 optp = OptionParser()
 optp.add_option('-v', '--verbose', dest='verbose', action='count',
                 help='Increase verbosity (specify multiple times for more)')
-optp.add_option('--exit-button', dest='exit_button', action='store_true')
+optp.add_option('--exit-button', dest='exit_button', action='store_true',
+                help='Add an exit button to the icon\'s context menu')
 opts, args = optp.parse_args()
 
 log_level = logging.WARNING  # default
